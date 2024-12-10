a100759f9483b28a19ff63a3272dd5ab819c01b1
Cristiano <137925557+Cristiano-gon@users.noreply.github.com>
2024-12-10 15:35:23 -0300

    Add files via upload
    Aula da escola Alura. React: desenvolvendo em JavaScript.

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,19 @@
+import { Fragment } from "react";
+import "./Banner.css";
+
+function Banner() {
+  // JSX
+  return (
+    <Fragment>
+       <header className="banner">
+          <img
+        src="/imagens/banner.png"
+        alt="O banner principal da página do Organo"
+          />
+        </header>
+    </Fragment>
+   
+  );
+}
+
+export default Banner;
